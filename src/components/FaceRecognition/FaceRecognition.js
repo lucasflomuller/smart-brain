0381b2c41f157bad8216b0d4ab273ad79a6719f2
lucasflomuller <lucasflorianomuller@gmail.com>
2019-04-12 12:04:39 +0000

    implemente multiple face recognition

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl, box }) => {
+const boxStyle = ({topRow, rightCol, bottomRow, leftCol}) => {
+  return {
+    top: topRow,
+    right: rightCol,
+    bottom: bottomRow,
+    left: leftCol
+  };
+}
+
+const FaceRecognition = ({ imageUrl, boxes }) => {
+  const boxesJSX = boxes.map(box => (
+    <div
+      key={`box${box.topRow}${box.rightCol}`}
+      className='bounding-box'
+      style={boxStyle(box)}>
+    </div>
+  ));
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
         <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
-        <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+        {boxesJSX}
       </div>
     </div>
   );
